8964c167d065c60a0279cf4bc667db89fe8bc04c
Austin Papritz <austin@papritz.dev>
2023-01-30 10:16:02 -0800

    basic UserContext made with user useState

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,1 +1,13 @@
 // TODO -- create the user provider!
+const { createContext, useState } = require('react');
+
+const { getUser } = createContext();
+
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser);
+
+  return <UserContext.Provider value={{ user, setUser }}> {children}</UserContext.Provider>;
+};
+
+export { UserProvider, UserContext };
